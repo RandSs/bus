d1977a831a354fe20696f51c00f62cf0afb4f275
RandSs <setif19raouf@yahoo.co.uk>
2020-06-11 18:57:40 +0200

    spelling mistake

--- bus_routine.rb
+++ bus_routine.rb
@@ -1,11 +1,11 @@
 class Bus
-    
+  
     def initialize(route_number, destiniation)
         @route_number = route_number
         @destiniation = destiniation
         @passenger = ["o", "p", "t", "y"]
     end
-
+   
     def number_of_passenger_in_the_bus()
       number_of_passenger = @passenger.length 
       puts "The bus contains #{number_of_passenger} passengers"
@@ -14,7 +14,7 @@ class Bus
      passenger.each {|passenger| @passenger.push(passenger)}
         picked_up_passenger = passenger.length
         number_of_passenger = @passenger.length
-        puts "Driver picked upp #{picked_up_passenger} more passengers so the bus now contains  #{number_of_passenger} passengers"
+        puts "Driver picked up #{picked_up_passenger} more passengers so the bus now contains  #{number_of_passenger} passengers"
     end
     def drop_off_pssenger(passenger)
         passenger.each {|passenger| @passenger.delete(passenger)}
